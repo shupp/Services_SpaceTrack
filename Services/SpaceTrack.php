8e4e8c0084dfeb92c584d8788cd8397c77b34100
Bill Shupp <hostmaster@shupp.org>
2014-03-05 15:10:12 -0800

    Some refactoring and documentation for tests

--- Services/SpaceTrack.php
+++ Services/SpaceTrack.php
@@ -1,8 +1,11 @@
 <?php
 
 /*
- * Example usage
+ * Services_SpaceTrack - a thin client for the space-track.org API
  *
+ * Example usage:
+ *
+ * <code>
  * $st = new Services_SpaceTrack(
  *     array(
  *          'username' => 'myusername',
@@ -17,14 +20,24 @@
  * }
  *
  * echo $tle;
+ * </code>
  *
  */
 
 require_once 'Services/SpaceTrack/Exception.php';
 
+/**
+ * Services_SpaceTrack 
+ * 
+ * @category  Services
+ * @package   SpaceTrack
+ * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @license   BSD
+ * @link      http://github.com/shupp/Services_SpaceTrack
+ */
 class Services_SpaceTrack
 {
-    protected $isAuthenticated = false;
+    protected $authenticated = false;
     protected $options = array(
         'url'        => 'https://www.space-track.org/',
         'username'   => '',
@@ -34,6 +47,13 @@ class Services_SpaceTrack
         'debug'      => false
     );
 
+    /**
+     * Constructor, allows for setting options
+     * 
+     * @param array $options Options to set
+     * 
+     * @return void
+     */
     public function __construct(array $options = array())
     {
         foreach ($options as $name => $val) {
@@ -41,12 +61,53 @@ class Services_SpaceTrack
         }
     }
 
+    /**
+     * Sets an option
+     * 
+     * @param string $name The option name
+     * @param mixed  $val  The option value
+     * 
+     * @return void
+     */
     public function setOption($name, $val)
     {
         // Probably should do some validation
         $this->options[$name] = $val;
     }
 
+    /**
+     * Getter for $this->authenticated.  Abstracted for testing.
+     * 
+     * @return bool
+     */
+    public function isAuthenticated()
+    {
+        return $this->authenticated;
+    }
+
+    /**
+     * Get an option
+     * 
+     * @param string $name The option name
+     * 
+     * @return mixed on success, null on failure
+     */
+    public function getOption($name)
+    {
+        // Probably should do some validation
+        if (isset($this->options[$name])) {
+            return $this->options[$name];
+        }
+        return null;
+    }
+
+    /**
+     * Shortcut for getting the latest TLE contents
+     * 
+     * @param int $noradID The NORAD ID of the satellite
+     * 
+     * @return string The TLE contents
+     */
     public function getLatestTLE($noradID = 25544)
     {
         return $this->sendRequest(
@@ -55,9 +116,18 @@ class Services_SpaceTrack
         );
     }
 
+    /**
+     * Sends a GET request to space-track.org.  Requires authenticate() to have
+     * been run first.
+     * 
+     * @param string $path The URI path
+     * 
+     * @return string The response contents
+     * @throws Services_SpaceTrack_Exception on failure
+     */
     public function sendRequest($path)
     {
-        if (!$this->isAuthenticated) {
+        if (!$this->isAuthenticated()) {
             $this->authenticate();
         }
 
@@ -77,9 +147,10 @@ class Services_SpaceTrack
         ob_end_clean();
 
         // Check response code
-        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $code = $this->getHTTPResponseCode($ch);
         if (strncmp($code, '2', 1)) {
-            // $this->cleanUp();
+            $this->authenticated = false;
+            $this->cleanUp();
             throw new Services_SpaceTrack_Exception(
                 "Unexpected response code: $code:\n" . $result,
                 $code
@@ -89,9 +160,15 @@ class Services_SpaceTrack
         return $result;
     }
 
+    /**
+     * Sends an authentication request and stores the cookie for future requests.
+     * 
+     * @return true on success or already authenticated
+     * @throws Services_SpaceTrack_Exception on error
+     */
     public function authenticate()
     {
-        if ($this->isAuthenticated) {
+        if ($this->isAuthenticated()) {
             return true;
         }
 
@@ -132,26 +209,28 @@ class Services_SpaceTrack
         ob_end_clean();
 
         // Check response code
-        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $code = $this->getHTTPResponseCode($ch);
         if (strncmp($code, '2', 1)) {
-            $this->cleanUp();
+            $this->cleanUp(); // Clean up on auth failures
             throw new Services_SpaceTrack_Exception(
                 "Unable to authenticate, received $code:\n" . $result,
                 $code
             );
         }
 
-        $this->isAuthenticated = true;
+        $this->authenticated = true;
 
         curl_close($ch);
+
+        return true;
     }
 
-    protected function sendCurlRequest($ch)
-    {
-        return curl_exec($ch);
-    }
-
-    protected function cleanUp()
+    /**
+     * Remove cookie file on exit for security
+     * 
+     * @return void
+     */
+    public function cleanUp()
     {
         // Remove cookie file
         if (strlen($this->options['cookieFile'])
@@ -161,6 +240,37 @@ class Services_SpaceTrack
         }
     }
 
+    /**
+     * Call curl_exec on the curl resource.  Abstracted for testing.
+     * 
+     * @param resource $ch The curl resource
+     * 
+     * @codeCoverageIgnore
+     * @return string The response content (using RETURNTRANSFER)
+     */
+    protected function sendCurlRequest($ch)
+    {
+        return curl_exec($ch);
+    }
+
+    /**
+     * Get the http respons code form the curl resource.  Abstracted for testing.
+     * 
+     * @param resource $ch The curl resource
+     * 
+     * @codeCoverageIgnore
+     * @return int The response code
+     */
+    protected function getHTTPResponseCode($ch)
+    {
+        return curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    }
+
+    /**
+     * Clean up cookie file on exit
+     * 
+     * @return void
+     */
     public function __destruct()
     {
         $this->cleanUp();
--- Services/SpaceTrack/Exception.php
+++ Services/SpaceTrack/Exception.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Services_SpaceTrack_Exception 
+ * 
+ * @uses      Exception
+ * @category  Services
+ * @package   SpaceTrack
+ * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @license   BSD
+ * @link      http://github.com/shupp/Services_SpaceTrack
+ */
 class Services_SpaceTrack_Exception extends Exception
 {
 }
